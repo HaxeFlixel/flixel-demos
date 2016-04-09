7678eeff61422c4100cf1ffe603672038f8a4988
Zaphod <zaphy@yandex.ru>
2016-04-09 16:35:05 +0300

    forgot to include FXAA class itself

---
+++ Effects/Filters/source/shaders/FXAA.hx
@@ -0,0 +1,68 @@
+package shaders;
+
+import openfl.display.Shader;
+
+/**
+ * ...
+ * @author MrCdK
+ */
+class FXAA extends Shader
+{
+	
+	@fragment var code = '
+	
+		vec2 delta = (1 / ${Shader.uTextureSize}.x, 1 / ${Shader.uTextureSize}.y);
+		
+		void main()
+		{
+			vec2 tuv = ${Shader.vTexCoord}.xy;
+			vec3 nw = texture2D(${Shader.uSampler}, tuv + vec2(-1, -1) * delta).rgb;
+			vec3 ne = texture2D(${Shader.uSampler}, tuv + vec2(1, -1) * delta).rgb;
+			vec3 sw = texture2D(${Shader.uSampler}, tuv + vec2(-1, 1) * delta).rgb;
+			vec3 se = texture2D(${Shader.uSampler}, tuv + vec2(1, 1) * delta).rgb;
+			vec4 texColor = texture2D(${Shader.uSampler}, tuv).rgba;
+			vec3 mid = texColor.rgb;
+			
+			vec3 lumA = vec3(0.299, 0.587, 0.114);
+			
+			float lumNW = nw.dot(lumA);
+			float lumNE = ne.dot(lumA);
+			float lumSW = sw.dot(lumA);
+			float lumSE = se.dot(lumA);
+			float lumMid = mid.dot(lumA);
+			
+			float lumMin = min(lumMid, min(min(lumNW, lumNE), min(lumSW, lumSE)));
+			float lumMax = max(lumMid, max(max(lumNW, lumNE), max(lumSW, lumSE)));
+			
+			vec2 dir;
+			dir.x = -((lumNW + lumNE) - (lumSW + lumSE));
+			dir.y = (lumNW + lumSW) - (lumNE + lumSE);
+			
+			float dirReduce = max((lumNW + lumNE + lumSW + lumSE) * (0.25 / 128), 1.0 / 8);
+			float rcpDirMin = 1 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
+			dir = min(vec2(8, 8), max(vec2(-8, -8), dir * rcpDirMin)) * delta;
+			
+			vec3 rgbA = 0.5 * (texture2D(${Shader.uSampler}, tuv + dir * (1.0 / 3.0 - 0.5)).xyz + texture2D(${Shader.uSampler}, tuv + dir * (2.0 / 3.0 - 0.5)).xyz);
+			vec3 rgbB = rgbA * 0.5 + 0.25 * (texture2D(${Shader.uSampler}, tuv + dir * -0.5).xyz + texture2D(${Shader.uSampler}, tuv + dir * 0.5).xyz);
+			float lumB = dot(rgbB, lumA);
+			vec4 color;
+			
+			if ((lumaB < lumaMin) || (lumaB > lumaMax))
+			{
+				color = vec4(rgbA, texColor.a);
+			}
+			else
+			{
+				color = vec4(rgbB, texColor.a);
+			}
+			
+			gl_FragColor = color;
+		}
+	';
+	
+	public function new() 
+	{
+		super();
+	}
+	
+}
