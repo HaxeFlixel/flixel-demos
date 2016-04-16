5158a4618432a4cac7989844f1ad4717de0100c0
Zaphod <zaphy@yandex.ru>
2016-04-16 23:52:47 +0300

    simple shaders demo, click on the stage to switch shaders

---
+++ Effects/SpriteShaders/source/Main.hx
@@ -0,0 +1,15 @@
+﻿package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+import openfl.display.Bitmap;
+import openfl.Assets;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(1024, 512, PlayState, 1));
+	}
+}
---
+++ Effects/SpriteShaders/source/shaders/Invert.hx
@@ -0,0 +1,24 @@
+package shaders;
+
+import openfl.display.Shader;
+
+/**
+ * ...
+ * @author MrCdK
+ */
+class Invert extends Shader
+{
+	@fragment var fragment = '
+
+	void main()
+	{
+		vec4 color = texture2D(${Shader.uSampler}, ${Shader.vTexCoord});
+		gl_FragColor = vec4(1.0 - color.r, 1.0 - color.g, 1.0 - color.b, color.a);
+	}
+	';
+	
+	public function new()
+	{
+		super();
+	}
+}
