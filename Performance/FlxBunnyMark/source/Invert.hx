5b23daa21f7d96df39b80a95717bc3f42d8ff0e6
Zaphod <zaphy@yandex.ru>
2016-04-17 17:08:07 +0300

    Fix for premultiplied alpha color in Invert shader

--- Performance/FlxBunnyMark/source/Invert.hx
+++ Performance/FlxBunnyMark/source/Invert.hx
@@ -13,7 +13,7 @@ class Invert extends Shader
 	void main()
 	{
 		vec4 color = texture2D(${Shader.uSampler}, ${Shader.vTexCoord});
-		gl_FragColor = vec4(1.0 - color.r, 1.0 - color.g, 1.0 - color.b, color.a);
+		gl_FragColor = vec4((1.0 - color.r) * color.a, (1.0 - color.g) * color.a, (1.0 - color.b) * color.a, color.a);
 	}
 	';
 	
