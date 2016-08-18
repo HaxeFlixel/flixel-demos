f4b45ab9df66aab177e1d152b9983fa8f979003b
Tommy Elfving <xerosugar@gmail.com>
2016-08-18 19:16:22 +0200

    1st commit for dialogue helper

---
+++ Features/DialogueHelper/source/Main.hx
@@ -0,0 +1,13 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(400, 300, PlayState));
+	}
+}
