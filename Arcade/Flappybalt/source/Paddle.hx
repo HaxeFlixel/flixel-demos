55f609525f28e84043881f3fd8fb1a2e118c44d6
Jens Fischer <jensfischer95@gmail.com>
2019-07-14 14:18:50 +0200

    Run haxe-formatter

--- Arcade/Flappybalt/source/Paddle.hx
+++ Arcade/Flappybalt/source/Paddle.hx
@@ -7,41 +7,40 @@ import flixel.FlxSprite;
 class Paddle extends FlxSprite
 {
 	public var targetY:Int = 0;
-	
+
 	inline static public var SPEED:Int = 480;
-	
+
 	public function new(X:Float = 0, Facing:Int = 0)
 	{
 		super(X, FlxG.height);
 		loadGraphic("assets/paddle.png", false);
-		
+
 		if (Facing == FlxObject.LEFT)
 		{
 			flipX = true;
 		}
 	}
-	
+
 	public function randomize():Void
 	{
 		targetY = Reg.PS.randomPaddleY();
-		
+
 		if (targetY < y)
 			velocity.y = -SPEED;
 		else
 			velocity.y = SPEED;
 	}
-	
+
 	override public function update(elapsed:Float):Void
 	{
-		if (((velocity.y < 0) && (y <= targetY + SPEED * elapsed)) ||
-			((velocity.y > 0) && (y >= targetY - SPEED * elapsed)))
+		if (((velocity.y < 0) && (y <= targetY + SPEED * elapsed)) || ((velocity.y > 0) && (y >= targetY - SPEED * elapsed)))
 		{
 			velocity.y = 0;
 			y = targetY;
 			Reg.PS.stopPaddleSound();
 			FlxG.sound.play("StoneStop");
 		}
-		
+
 		super.update(elapsed);
 	}
 }
