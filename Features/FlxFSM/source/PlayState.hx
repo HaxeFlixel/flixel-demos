1185f636bc69a2c56e5121e9285a803afd6565be
George FunBook <gkurelic@gmail.com>
2023-08-15 14:19:36 -0500

    fix jump after superjump pickup, add groundpound pickup

--- Features/FlxFSM/source/PlayState.hx
+++ Features/FlxFSM/source/PlayState.hx
@@ -5,17 +5,24 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.tile.FlxTilemap;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
 using StringTools;
 
 class PlayState extends FlxState
 {
+	static inline var BASE_INFO:String = "LEFT & RIGHT to move, UP to jump";
+	static inline var GROUND_POUND_INFO:String = "DOWN (in the air) to ground-pound";
+	static inline var RESET_INFO:String = "R to Reset\n\nCurrent State: {STATE}";
+	
 	var _map:FlxTilemap;
 	var _slime:Slime;
-	var _powerup:FlxSprite;
+	var _superJump:FlxSprite;
+	var _groundPound:FlxSprite;
 
-	var _info:String = "LEFT & RIGHT to move, UP to jump\nDOWN (in the air) " + "to ground-pound.\nR to Reset\n\nCurrent State: {STATE}";
+	var _info:String = BASE_INFO + "\n" + RESET_INFO;
 	var _txtInfo:FlxText;
 
 	override public function create():Void
@@ -23,8 +30,10 @@ class PlayState extends FlxState
 		bgColor = 0xff661166;
 		super.create();
 
-		_map = new FlxTilemap();
-		_map.loadMapFromArray([
+		final J = 99;
+		final G = 100;
+		final columns = 20;
+		final data = [
 			1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
 			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -35,19 +44,35 @@ class PlayState extends FlxState
 			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
 			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
 			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
-			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+			1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, G, 0, 1,
 			1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
 			1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-			1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+			1, 0, 0, J, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
 			1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-		], 20, 15, "assets/tiles.png", 16, 16);
+		];
+		
+		// remove powerups from map, store index so we can place them
+		final superJumpIndex = data.indexOf(J);
+		data[superJumpIndex] = 0;
+		
+		final groundPoundIndex = data.indexOf(G);
+		data[groundPoundIndex] = 0;
+		
+		final tileSize = 16;
+		_map = new FlxTilemap();
+		_map.loadMapFromArray(data, 20, 15, "assets/tiles.png", tileSize, tileSize);
 		add(_map);
 
 		_slime = new Slime(192, 128);
 		add(_slime);
 
-		_powerup = new FlxSprite(48, 208, "assets/powerup.png");
-		add(_powerup);
+		_superJump = new FlxSprite((superJumpIndex % columns) * tileSize, Std.int(superJumpIndex / columns) * tileSize, "assets/powerup.png");
+		add(_superJump);
+
+		_groundPound = new FlxSprite((groundPoundIndex % columns) * tileSize, Std.int(groundPoundIndex / columns) * tileSize, "assets/powerup.png");
+		_groundPound.flipY = true;
+		_groundPound.y += 4;
+		add(_groundPound);
 
 		_txtInfo = new FlxText(16, 16, -1, _info);
 		add(_txtInfo);
@@ -58,7 +83,19 @@ class PlayState extends FlxState
 		super.update(elapsed);
 
 		FlxG.collide(_map, _slime);
-		FlxG.overlap(_slime, _powerup, getPowerup);
+		
+		FlxG.overlap(_slime, _superJump, function (_, _)
+		{
+			_slime.addSuperJump();
+			_superJump.kill();
+		});
+		
+		FlxG.overlap(_slime, _groundPound, function (_, _)
+		{
+			_slime.addGroundPound();
+			_groundPound.kill();
+			startGroundPoundInfoTween();
+		});
 
 		_txtInfo.text = _info.replace("{STATE}", Type.getClassName(_slime.fsm.stateClass));
 
@@ -67,12 +104,27 @@ class PlayState extends FlxState
 			FlxG.camera.flash(FlxColor.BLACK, .1, FlxG.resetState);
 		}
 	}
-
-	function getPowerup(slime:Slime, particle:FlxSprite):Void
+	
+	function startGroundPoundInfoTween()
 	{
-		slime.fsm.transitions.replace(Slime.Jump, Slime.SuperJump);
-		slime.fsm.transitions.add(Slime.Jump, Slime.Idle, Slime.Conditions.grounded);
-
-		particle.kill();
+		final duration = 2.0;
+		final flash_period = 0.15 / duration;
+		final oldColor = _txtInfo.color;
+		final ease = FlxEase.linear;
+		FlxTween.num(0, 1, duration, 
+			{
+				onComplete:function (_)
+				{
+					_txtInfo.color = oldColor;
+					_info = BASE_INFO + "\n" + GROUND_POUND_INFO + "\n" + RESET_INFO;
+				}
+			},
+			function(n)
+			{
+				_txtInfo.color = ((n / flash_period) % 1 > 0.5) ? FlxColor.YELLOW : oldColor;
+				final chars = Std.int(ease(n) * GROUND_POUND_INFO.length);
+				_info = BASE_INFO + "\n" + GROUND_POUND_INFO.substr(0, chars) + "\n" + RESET_INFO;
+			}
+		);
 	}
 }
