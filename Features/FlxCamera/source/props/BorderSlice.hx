431ce75fab6abf681713a326da4ac06e5d80e357
George FunBook <gkurelic@gmail.com>
2023-08-31 10:44:44 -0500

    more slice optimizations

--- Features/FlxCamera/source/props/BorderSlice.hx
+++ Features/FlxCamera/source/props/BorderSlice.hx
@@ -8,10 +8,11 @@ abstract BorderSlice(FlxSliceSprite) from FlxSliceSprite to FlxSliceSprite
 {
 	inline public function new (x = 0.0, y = 0.0, width:Float, height:Float)
 	{
-		this = new FlxSliceSprite("assets/Border.png", new FlxRect(15, 15, 20, 20), width, height);
+		this = new FlxSliceSprite("assets/BorderSlice.png", new FlxRect(15, 15, 20, 20), width, height);
 		this.x = x;
 		this.y = y;
 		// reduce vertice counts by stretching rather than tiling
+		this.fillCenter = false;
 		this.stretchBottom = true;
 		this.stretchTop = true;
 		this.stretchLeft = true;
